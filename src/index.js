dfa2695bc7498cc160b444c09547b2484256c6d8
Ajbar Imad <ajbar.imad.99@gmail.com>
2022-04-09 00:15:57 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -9,3 +9,13 @@ ReactDOM.render(
     </React.StrictMode>
     ,document.getElementById("root")
 )
+
+const person ={
+    name : "imad ajbar",
+    age : 29
+}
+
+// this call object destructuring
+// syntax : const {property} = object
+// const {name, age} = person
+// console.log(name, age)
