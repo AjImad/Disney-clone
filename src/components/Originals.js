450167fb9d2949187ccc2826130914ceb3831a75
Ajbar Imad <ajbar.imad.99@gmail.com>
2022-04-22 22:52:38 +0000

    Modifying Home page by adding new components to it

---
+++ src/components/Originals.js
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+const Originals = props => {
+    return (
+        <Container>
+            <h4>Originals</h4>
+            <Content>
+                <Wrap>
+                    <Link to='/'>
+                        <img src='https://lumiere-a.akamaihd.net/v1/images/p_insideout_19751_af12286c.jpeg' alt='' />
+                    </Link>
+                </Wrap>
+                <Wrap>
+                    <Link to='/'>
+                        <img src='https://lumiere-a.akamaihd.net/v1/images/p_insideout_19751_af12286c.jpeg' alt='' />
+                    </Link>
+                </Wrap>
+                <Wrap>
+                    <Link to='/'>
+                        <img src='https://lumiere-a.akamaihd.net/v1/images/p_insideout_19751_af12286c.jpeg' alt='' />
+                    </Link>
+                </Wrap>
+                <Wrap>
+                    <Link to='/'>
+                        <img src='https://lumiere-a.akamaihd.net/v1/images/p_insideout_19751_af12286c.jpeg' alt='' />
+                    </Link>
+                </Wrap>
+            </Content>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    padding: 0 0 26px;
+`;
+
+const Content = styled.div`
+    padding-top: 25px;
+    display: grid;
+    gap: 25px;
+    grid-template-columns: repeat(4, minmax(0,1fr));
+    
+    @media (max-width: 760px) {
+       grid-template-columns: repeat(2, minmax(0,1fr))
+    }
+`;
+
+const Wrap = styled.div`
+    padding-top: 56.25%;
+    border-radius: 10px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    overflow: hidden;
+    position: relative;
+    border: 3px solid rgba(249, 249, 249, 0.1);
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+    img{
+        inset: 0px;
+        position: absolute;
+        object-fit: cover;
+        display: block;
+        height: 100%;
+        width: 100%;
+        z-index: 1;
+        top: 0;
+        opacity: 1;
+        transition: opacity 500ms ease-in-out 0s;
+    }
+
+    &:hover {
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+        rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        transform: scale(1.02);
+        border-color: rgba(249, 249, 249, 0.8);
+        opacity: 0.8
+    }
+`;
+
+export default Originals;
